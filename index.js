26988a9edca4d2099f5fa2161dd938c6cfa4aa54
Chobby12 <omenyitk@gmail.com>
2026-03-02 17:32:06 +0100

    cors

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express")
 const socket = require("socket.io")
+const cors = require("cors")
 
 const app = express()
 
 const port = process.env.PORT || 3000
 
+app.use(cors())
 app.use(express.static("public"))
 
 const server = app.listen(port, err => {
